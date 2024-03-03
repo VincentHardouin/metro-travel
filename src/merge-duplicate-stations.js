701f03e45eeff798395c3112241d3ad5f41cd53a
Vincent Hardouin <vincenthardouin@icloud.com>
2024-03-03 18:03:08 +0100

    feat: improve merge-duplicate-stations to handle line direction

--- src/merge-duplicate-stations.js
+++ src/merge-duplicate-stations.js
@@ -1,33 +1,47 @@
 import fs from 'node:fs';
 
-const metros = JSON.parse(fs.readFileSync('./assets/export.geojson'));
+const exportData = JSON.parse(fs.readFileSync('./assets/export.geojson'));
 
 const stations = new Map()
 
-metros.features
+const metros = exportData.features
   .filter(d => {
     return d.properties.railway === 'stop' ||
       (d.properties.public_transport === 'stop_position' && (d.properties["type:RATP"] === 'metro' || d.properties.subway === 'yes'))
-  }).forEach(s => {
+  })
+
+for (const s of metros) {
   const properties = s.properties;
   const stationName = properties.name;
 
   const currentCoordinates = s.geometry.coordinates;
-  const currentLine = s.properties['@relations']?.at(0)?.reltags.ref;
 
-  if (stations.has(stationName)) {
-    const station = stations.get(stationName);
-    station.properties.coordinates.push(currentCoordinates);
-    if (currentLine) station.properties.lines.add(currentLine)
-  } else {
-    s.properties.coordinates = [currentCoordinates];
-    s.properties.lines = new Set();
+  const relations =  s.properties['@relations'];
 
-    if (currentLine) s.properties.lines.add(currentLine);
-
-    stations.set(stationName, s);
+  if (!relations) {
+    continue;
   }
-});
+
+  relations.forEach((relation) => {
+    const lineRef = relation.reltags.ref;
+    const lineFrom = relation.reltags.from;
+    const lineTo = relation.reltags.to;
+    const currentLine = `${lineRef} - ${lineFrom} -> ${lineTo}`;
+
+    if (stations.has(stationName)) {
+      const station = stations.get(stationName);
+      station.properties.coordinates.set(JSON.stringify(currentCoordinates), currentCoordinates);
+      if (currentLine) station.properties.lines.add(currentLine)
+    } else {
+      s.properties.coordinates = new Map();
+      s.properties.coordinates.set(JSON.stringify(currentCoordinates), currentCoordinates);
+      s.properties.lines = new Set();
+      if (currentLine) s.properties.lines.add(currentLine);
+
+      stations.set(stationName, s);
+    }
+  })
+}
 
 
 
@@ -38,6 +52,7 @@ const outputGeojson = {
 
 stations.forEach((s, key) => {
   s.properties.lines = Array.from(s.properties.lines);
+  s.properties.coordinates = Array.from(s.properties.coordinates.values());
   outputGeojson.features.push(s);
 });
 
