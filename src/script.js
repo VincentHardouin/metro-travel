b70e12c1cf027cce84cd113907c53a89f688ac12
Vincent Hardouin <vincenthardouin@icloud.com>
2024-03-03 18:19:05 +0100

    feat: show path between adjacent path

--- src/script.js
+++ src/script.js
@@ -2,8 +2,10 @@ import * as d3 from "d3";
 
 import arrondissements from "../assets/arrondissements.geojson"
 import stationsData from "../assets/stations.geojson"
+import linesData from "../assets/lines.geojson"
 
 const stations = stationsData.features;
+const lines = linesData.features;
 
 const svg = d3.select("svg"),
   width = +svg.attr("width"),
@@ -12,7 +14,7 @@ const svg = d3.select("svg"),
 const projection = d3.geoMercator()
   .center([2.3522, 48.8566])
   .scale(200000)
-  .translate([ width/2, height/2 ])
+  .translate([width / 2, height / 2])
 
 const tooltip = d3.select('body').append('div')
   .attr('class', 'tooltip')
@@ -46,36 +48,11 @@ function createStationsList() {
   document.getElementById('stations').innerHTML = stationsList;
 }
 
-function printAllStations() {
-  svg.selectAll('.metro-station')
-    .data(stations)
-    .enter()
-    .append('circle')
-    .attr('class', 'metro-station')
-    .attr('cx', function(d) { return projection(d.geometry.coordinates)[0]; })
-    .attr('cy', function(d) { return projection(d.geometry.coordinates)[1]; })
-    .attr('r', 3)
-    .style("fill", "red")
-    .on('mouseover', function(e, d) {
-      tooltip.style('visibility', 'visible');
-      tooltip.html(d.properties.name);
-    })
-    .on('mousemove', function(event) {
-      const [x, y] = d3.pointer(event)
-      tooltip.style('top', (y - 10) + 'px')
-        .style('left', (x + 10) + 'px');
-    })
-    .on('mouseout', function() {
-      tooltip.style('visibility', 'hidden');
-    });
-}
-
 function addStation(stationName) {
   if (addedStations.has(stationName)) {
     return;
   }
   const station = stations.find(d => d.properties.name === stationName);
-  addedStations.set(stationName, station);
 
   svg.append('circle')
     .attr('class', 'metro-station')
@@ -83,22 +60,64 @@ function addStation(stationName) {
     .attr('cy', projection(station.geometry.coordinates)[1])
     .attr('r', 3)
     .style("fill", "red")
-    .on('mouseover', function(e, d) {
+    .on('mouseover', function (e, d) {
       tooltip.style('visibility', 'visible');
       tooltip.html(station.properties.name);
     })
-    .on('mousemove', function(event) {
+    .on('mousemove', function (event) {
       const [x, y] = d3.pointer(event)
       tooltip.style('top', (y - 10) + 'px')
         .style('left', (x + 10) + 'px');
     })
-    .on('mouseout', function() {
+    .on('mouseout', function () {
       tooltip.style('visibility', 'hidden');
     });
+
+  addPathBetweenStations(station, addedStations, lines);
+
+  addedStations.set(stationName, station);
+}
+
+function addPathBetweenStations(newStation, addedStations, lines) {
+  for (const newStationLine of newStation.properties.lines) {
+    const adjacentStations = newStation.properties.adjacentStations[newStationLine];
+    for (const adjacentStationName of adjacentStations) {
+      if (!addedStations.has(adjacentStationName)) {
+        continue;
+      }
+
+      const adjacentStation = addedStations.get(adjacentStationName);
+      const filteredLines = lines
+        .filter(l => l.properties.name === newStationLine)
+      for (const line of filteredLines) {
+        const lineCoordinates = line.geometry.type === 'LineString' ? line.geometry.coordinates : line.geometry.coordinates[0];
+
+        const newStationIndex = newStation.properties.inLineIndex[newStationLine];
+        const adjacentStationIndex = adjacentStation.properties.inLineIndex[newStationLine];
+
+        if (lineCoordinates.length < newStationIndex && lineCoordinates.length < adjacentStationIndex) {
+          continue;
+        }
+
+        let drawLine;
+        if (newStationIndex < adjacentStationIndex) {
+          drawLine = lineCoordinates.slice(newStationIndex, adjacentStationIndex + 1);
+        } else {
+          drawLine = lineCoordinates.slice(adjacentStationIndex, newStationIndex + 1);
+        }
+
+        svg.append('path')
+          .attr('d', d3.line()(drawLine.map(c => projection(c))))
+          .attr('stroke', 'black')
+          .attr('stroke-width', 2)
+          .attr('fill', 'none');
+      }
+    }
+  }
 }
 
 function handleClickOnTryButton() {
-  document.getElementById('try').addEventListener('click', function() {
+  document.getElementById('try').addEventListener('click', function () {
     const stationName = document.getElementById('station').value;
     addStation(stationName);
   });
