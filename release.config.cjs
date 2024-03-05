f2463ed724f99fa1826c356b6fcf8f272db99a86
Vincent Hardouin <vincenthardouin@icloud.com>
2024-03-05 22:13:51 +0100

    feat: add cd workflow

---
+++ release.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  "branches": [
+    "main"
+  ],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/CHANGELOG.md"
+      }
+    ],
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        "assets": [
+          "package.json",
+          "package-lock.json",
+          "docs/CHANGELOG.md"
+        ],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ]
+}
