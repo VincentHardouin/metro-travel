1a6f333d6d338293f790c559cbebb6130a9134df
Vincent Hardouin <vincenthardouin@icloud.com>
2024-03-11 12:57:31 +0100

    refactor: export method for extract-lines script

--- src/extract-lines.js
+++ src/extract-lines.js
@@ -1,18 +1,20 @@
 import fs from 'node:fs';
 
-const metros = JSON.parse(fs.readFileSync('./assets/export.geojson'));
+export function extractLines({ inputPath = './assets/export.geojson', outputPath = 'lines.geojson' }) {
+  const metros = JSON.parse(fs.readFileSync(inputPath));
 
-const lines = metros.features.filter((d) => {
-  return d.geometry.type === 'MultiLineString' || (d.geometry.type === 'LineString' && d.properties.route === 'subway');
-});
+  const lines = metros.features.filter((d) => {
+    return d.geometry.type === 'MultiLineString' || (d.geometry.type === 'LineString' && d.properties.route === 'subway');
+  });
 
-const outputGeojson = {
-  type: 'FeatureCollection',
-  features: [],
-};
+  const outputGeojson = {
+    type: 'FeatureCollection',
+    features: [],
+  };
 
-lines.forEach((l) => {
-  outputGeojson.features.push(l);
-});
+  lines.forEach((l) => {
+    outputGeojson.features.push(l);
+  });
 
-fs.writeFileSync('lines.geojson', JSON.stringify(outputGeojson, null, 2));
+  fs.writeFileSync(outputPath, JSON.stringify(outputGeojson, null, 2));
+}
