bc0b495ed28763ed388cab66d8a0c7feb4b6e67a
Vincent Hardouin <vincenthardouin@icloud.com>
2024-03-11 12:55:45 +0100

    refactor: export method for merge-duplicate-stations script

--- src/merge-duplicate-stations.js
+++ src/merge-duplicate-stations.js
@@ -1,56 +1,58 @@
 import fs from 'node:fs';
 
-const exportData = JSON.parse(fs.readFileSync('./assets/export.geojson'));
+export function mergeDuplicateStations({ inputPath = './assets/export.geojson', outputPath = 'stations.geojson' }) {
+  const exportData = JSON.parse(fs.readFileSync(inputPath, 'utf-8'));
 
-const stations = new Map();
+  const stations = new Map();
 
-const metros = exportData.features
-  .filter((d) => {
-    return d.properties.railway === 'stop'
-      || (d.properties.public_transport === 'stop_position' && (d.properties['type:RATP'] === 'metro' || d.properties.subway === 'yes'));
+  const metros = exportData.features
+    .filter((d) => {
+      return d.properties.railway === 'stop'
+        || (d.properties.public_transport === 'stop_position' && (d.properties['type:RATP'] === 'metro' || d.properties.subway === 'yes'));
+    });
+
+  for (const s of metros) {
+    const properties = s.properties;
+    const stationName = properties.name;
+
+    const currentCoordinates = s.geometry.coordinates;
+
+    const relations = s.properties['@relations'];
+
+    if (!relations)
+      continue;
+
+    relations.forEach((relation) => {
+      const currentLine = relation.reltags.name;
+
+      if (stations.has(stationName)) {
+        const station = stations.get(stationName);
+        station.properties.coordinates.set(JSON.stringify(currentCoordinates), currentCoordinates);
+        if (currentLine)
+          station.properties.lines.add(currentLine);
+      }
+      else {
+        s.properties.coordinates = new Map();
+        s.properties.coordinates.set(JSON.stringify(currentCoordinates), currentCoordinates);
+        s.properties.lines = new Set();
+        if (currentLine)
+          s.properties.lines.add(currentLine);
+
+        stations.set(stationName, s);
+      }
+    });
+  }
+
+  const outputGeojson = {
+    type: 'FeatureCollection',
+    features: [],
+  };
+
+  stations.forEach((s) => {
+    s.properties.lines = Array.from(s.properties.lines);
+    s.properties.coordinates = Array.from(s.properties.coordinates.values());
+    outputGeojson.features.push(s);
   });
 
-for (const s of metros) {
-  const properties = s.properties;
-  const stationName = properties.name;
-
-  const currentCoordinates = s.geometry.coordinates;
-
-  const relations = s.properties['@relations'];
-
-  if (!relations)
-    continue;
-
-  relations.forEach((relation) => {
-    const currentLine = relation.reltags.name;
-
-    if (stations.has(stationName)) {
-      const station = stations.get(stationName);
-      station.properties.coordinates.set(JSON.stringify(currentCoordinates), currentCoordinates);
-      if (currentLine)
-        station.properties.lines.add(currentLine);
-    }
-    else {
-      s.properties.coordinates = new Map();
-      s.properties.coordinates.set(JSON.stringify(currentCoordinates), currentCoordinates);
-      s.properties.lines = new Set();
-      if (currentLine)
-        s.properties.lines.add(currentLine);
-
-      stations.set(stationName, s);
-    }
-  });
+  fs.writeFileSync(outputPath, JSON.stringify(outputGeojson, null, 2));
 }
-
-const outputGeojson = {
-  type: 'FeatureCollection',
-  features: [],
-};
-
-stations.forEach((s) => {
-  s.properties.lines = Array.from(s.properties.lines);
-  s.properties.coordinates = Array.from(s.properties.coordinates.values());
-  outputGeojson.features.push(s);
-});
-
-fs.writeFileSync('stations.geojson', JSON.stringify(outputGeojson, null, 2));
