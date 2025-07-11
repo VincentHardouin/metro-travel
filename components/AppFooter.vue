3ce9bb7af5b9b86499de4bd6e5022f5a5d128a32
Vincent Hardouin <vincenthardouin@icloud.com>
2025-07-11 16:53:53 +0200

    feat: add default layout

---
+++ components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer
+      class="fixed bottom-2 left-1/2 transform -translate-x-1/2
+           text-gray-500 dark:text-gray-400
+           text-sm
+           bg-white/80 dark:bg-gray-800/80
+           backdrop-blur-lg px-4 py-1 rounded-full shadow
+           max-w-[90%] w-fit
+           z-50
+           transition-colors duration-300"
+  >
+    © 2025 Metro Travel – par Vincent Hardouin
+  </footer>
+</template>
